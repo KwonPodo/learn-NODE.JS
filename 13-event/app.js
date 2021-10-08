541b93a6be58e72f8ceae9893b99890378c1b00c
ludin9 <ludin9901@gmail.com>
2021-10-08 12:41:26 +0900

    blank

---
+++ 13-event/app.js
@@ -0,0 +1,17 @@
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+const callback1 = (args) => {
+  console.log("first callback -", args);
+};
+
+emitter.on("jay", callback1);
+
+emitter.on("jay", (args) => {
+  console.log("second callback - ", args);
+});
+
+emitter.emit("jay", { message: 1 });
+emitter.emit("jay", { message: 2 });
+emitter.removeListener("jay", callback1);
+emitter.emit("jay", { message: 3 });
---
+++ 13-event/main.js
@@ -0,0 +1,5 @@
+const logger = require("./logger.js");
+
+logger.log(() => {
+  console.log(".....doing something!");
+});
